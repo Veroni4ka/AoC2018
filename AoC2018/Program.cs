679f4746958e0f86eac8340fa93e9e38bd25cc4f
Veronika Kolesnikova <veronika.kolesnikova@yahoo.com>
2018-12-05 01:33:46 -0500

    Days 3-5 - lots of mess

--- AoC2018/Program.cs
+++ AoC2018/Program.cs
@@ -15,7 +15,20 @@ namespace AoC2018
 
             Day2 day2 = new Day2();
             //day2.Day2Task1();
-            day2.Day2Task2();
+            //day2.Day2Task2();
+
+            Day3 day3 = new Day3();
+            //day3.Day3Task1();
+            //day3.Day3Task2();
+
+            Day4 day4 = new Day4();
+            //day4.Day4Task1();
+
+            Day4Sol day4sol = new Day4Sol();
+            //day4sol.Main();
+
+            Day5 day5 = new Day5();
+            day5.Day5Task2();
         }
     }
 }
